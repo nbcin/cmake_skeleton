52932e1c822125027c67f3b337ecafb98035c485
Jean Guyomarc'h <jeanguyomarch.github@gmail.com>
2013-09-06 15:29:15 +0200

    Tmp bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,22 @@
 #!/bin/sh -e
 
+
 if [ -d build ]; then
    rm -Rf build;
 fi
 
 mkdir -p build
 cd build
+
+if [ "x$1" == "x--doc" ]; then
+   tmp="/tmp/tmp2LqYfht9Ee"
+   cmake ..
+   make doc
+   mkdir $tmp
+   cp -R doc/html $tmp/
+
+   exit 0
+fi
 
 if [ "x$1" == "x" ]; then
    cmake ..
